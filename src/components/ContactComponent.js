cee9ab13c424d70657774d04b2830e195f0a84df
Maxence <lemairemaxence@hotmail.com>
2021-03-19 18:56:38 +0100

    contact ok

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,45 +1,50 @@
 import React from 'react'
+import Fade from 'react-reveal/Fade';
 
 export default function ContactComponent() {
     return (
         <div id="sectionContact">
-            {/* LEFT */}
-            <div id="divContactLeft">
-                <div id="divNameMail">
-                    <div className="nameMail">
-                        <p>Name</p>
-                        <input type="text"/>
+            <Fade right><h1>Say hello,</h1></Fade>
+            <div id="twoDivs">
+                {/* LEFT */}
+                <Fade>
+                <div id="divContactLeft">
+                    <div id="divNameMail">
+                        <div className="nameMail">
+                            <p>Name</p>
+                            <input type="text"/>
+                        </div>
+                        <div className="nameMail">
+                            <p>E-mail</p>
+                            <input type="text" placeholder="example@hotmail.com"/>
+                        </div>
                     </div>
-                    <div className="nameMail">
-                        <p>E-mail</p>
-                        <input type="text" placeholder="example@hotmail.com"/>
+                    <div id="divMessage">
+                        <p>Message</p>
+                        <textarea name="" id="" cols="30" rows="15"></textarea>
                     </div>
-                </div>
-                <div id="divMessage">
-                    <p>Message</p>
-                    <textarea name="" id="" cols="30" rows="10"></textarea>
-                </div>
-                <div id="divSubmit">
-                    <button id="buttonSubmit">Submit</button>
-                </div>
-            </div>
-            {/* RIGHT */}
-            <div id="divContactRight">
-                <div id="contactAdress">
-                    <i class="fas fa-map-pin"></i>
-                    <div>
-                        <p>Rue des Trois Rois, 146</p>
-                        <p>1180 Brussels</p>
+                    <div id="divSubmit">
+                        <button id="buttonSubmit">Submit</button>
                     </div>
                 </div>
-                <div id="contactPhone">
-                    <i class="fas fa-phone-alt"></i>
-                    <p>+32 478/73.29.40</p>
+                </Fade>
+                {/* RIGHT */}
+                <Fade>
+                <div id="divContactRight">
+                    <div className="contactPart">
+                        <i class="fas fa-map-pin"></i>
+                        <p id="pAdress">Rue des Trois Rois, 146 - 1180 Brussels</p>
+                    </div>
+                    <div className="contactPart">
+                        <i class="fas fa-phone-alt"></i>
+                        <p>+32 478 73 29 40</p>
+                    </div>
+                    <div className="contactPart">
+                        <i class="fas fa-envelope"></i>
+                        <p id="pMail">lemairemaxence@hotmail.com</p>
+                    </div>
                 </div>
-                <div id="contactMail">
-                    <i class="fas fa-envelope"></i>
-                    <p>lemairemaxence@hotmail.com</p>
-                </div>
+                </Fade>
             </div>
         </div>
     )
